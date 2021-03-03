5ad3e93b89d1c1649c1884fee0f2018315330740
maxsherepot <dominator5530@gmail.com>
2021-03-03 12:47:38 +0200

    album info buttons added

--- client/src/pages/idPage/AlbumInfo.js
+++ client/src/pages/idPage/AlbumInfo.js
@@ -16,11 +16,47 @@ const AlbumInfo = ({ album }) => {
         </div>
 
         <div className="col-8 text-light pt-5 pl-5">
-          <h4 className="card-title specialColor">{album.title}</h4>
-          <h5 className="">{album.author}</h5>
-          <div>{album.year}</div>
-          <div>14 tracks</div>
-          <div> 41 minutes</div>
+
+          <div className="AlbumInformation">
+            <h4 className="card-title specialColor">{album.title}</h4>
+            <h5 className="">{album.author}</h5>
+            <div>{album.year}</div>
+            <div>14 tracks</div>
+            <div> 41 minutes</div>
+          </div>
+
+          <div className="AlbumButtons pt-4">
+            <div>Listen on:</div>
+
+            <div className="d-flex justify-content-between">
+              <div className="socialButtons">
+                <button className="btn p-0 albumButton">
+                  <img
+                    className="img-fluid"
+                    src="https://music.youtube.com/img/on_platform_logo_dark.svg"
+                    alt="..."
+                  />
+                </button>
+
+                <button className="btn p-0 albumButton">
+                  <img
+                    className="img-fluid"
+                    src="https://hantavirusofficial.com/wp-content/uploads/2019/12/Spotify_Logo_RGB_White.png"
+                    alt="..."
+                  />
+                </button>
+              </div>
+
+              <div className="toFavoriteButton">
+                <button
+                  className="btn btn-dark specialColor favoriteButton">
+                  Add to favorites
+              </button>
+              </div>
+            </div>
+          </div>
+
+
         </div>
 
       </div>
